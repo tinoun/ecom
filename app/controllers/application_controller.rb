d339e38bb982bc6c2b0c8b05ad2692294a714c9b
Manu <manujouve@gmail.com>
2018-05-29 12:32:10 +0200

    Devise Config Done

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,9 @@
 class ApplicationController < ActionController::Base
+
+  before_action :configure_devise_parameters, if: :devise_controller?
+
+  def configure_devise_parameters
+
+    devise_parameters_sanitizer.permit(:sign_up) { |u| u.permit(:name, :email, :password, :password_confirmation)}
+  end
 end
